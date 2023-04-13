36599bdb00bdd8a0a529ba4a35496db1f3166dab
Bruno Almeida <brunoalmeidasantos93@gmail.com>
2023-04-13 11:25:52 -0300

    first commit

---
+++ src/contexts/TransactionContext.tsx
@@ -0,0 +1,72 @@
+import { ReactNode, createContext, useEffect, useState } from "react"
+import { api } from "../lib/axios"
+
+interface Transaction {
+  id: number
+  description: string
+  type: "income" | "outcome"
+  category: string
+  price: number
+  createdAt: string
+}
+
+interface CreateTransactionInput {
+  description: string
+  price: number
+  type: "income" | "outcome"
+  category: string
+}
+
+interface TransactionContextType {
+  transactions: Transaction[]
+  fetchTransactions: (query?: string) => Promise<void>
+  createTransaction: (data: CreateTransactionInput) => Promise<void>
+}
+
+interface TransactionProviderProps {
+  children: ReactNode
+}
+
+export const TransactionsContext = createContext({} as TransactionContextType)
+
+export function TransactionProvider({ children }: TransactionProviderProps) {
+  const [transactions, setTransactions] = useState<Transaction[]>([])
+
+  async function fetchTransactions(query?: string) {
+    const response = await api.get("/transactions", {
+      params: {
+        _sort: "createdAt",
+        _order: "desc",
+        q: query,
+      },
+    })
+
+    setTransactions(response.data)
+  }
+
+  async function createTransaction(data: CreateTransactionInput) {
+    const { description, price, category, type } = data
+
+    const response = await api.post("/transactions", {
+      description,
+      price,
+      category,
+      type,
+      createdAt: new Date(),
+    })
+
+    setTransactions((state) => [response.data, ...state])
+  }
+
+  useEffect(() => {
+    fetchTransactions()
+  }, [])
+
+  return (
+    <TransactionsContext.Provider
+      value={{ transactions, fetchTransactions, createTransaction }}
+    >
+      {children}
+    </TransactionsContext.Provider>
+  )
+}
